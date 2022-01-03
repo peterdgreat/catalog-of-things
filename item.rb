25d8a7d118cb89aba875e2d7d68be6982e2635b5
DyaryRaoof <dyary2020@gmail.com>
2022-01-04 00:48:46 +0300

    Add item.rb file with class inside

---
+++ item.rb
@@ -0,0 +1,17 @@
+class Item 
+    def initialize genre, author, source, label, publish_date, archived
+        @id = Random.rand(1..1000000)
+        @genre = genre
+        @author = author
+        @source = source
+        @label = label
+        @publish_date = publish_date
+        @archived = archived
+    end
+
+    def can_be_archived
+    end
+
+    def move_to_archive
+    end
+end
