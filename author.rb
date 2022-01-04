fe78184c074d1080c936d4d1a0150e0623111384
Ajayi Peter <peterdgreat2016@gmail.com>
2022-01-04 17:07:47 +0100

    All class properties setup in constructor

--- author.rb
+++ author.rb
@@ -2,8 +2,9 @@ class Author
   attr_reader :id
   attr_accessor :name, :items
 
-  def initialize(name)
-    @name = name
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
     @items = []
     @id = Random.rand(1..1_000_000)
   end
--- game.rb
+++ game.rb
@@ -1,2 +1,8 @@
 class Game
+  attr_accessor :multi_player, :last_played_at
+
+  def initialize(multi_player, last_played_at)
+    @multi_player = multi_player
+    @last_played_at = last_played_at
+  end
 end
