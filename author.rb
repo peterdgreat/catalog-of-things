3d1e112df3e2eb4d1f661128ba194efb88a1b076
Ajayi Peter <peterdgreat2016@gmail.com>
2022-01-04 18:22:20 +0100

    can be archieved method implemented

--- author.rb
+++ author.rb
@@ -1,11 +1,17 @@
 class Author
   attr_reader :id
-  attr_accessor :name, :items
+  attr_accessor :name, :items, :last_played_at, :publish_date, :archived
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name, publish_date, archived: false)
     @first_name = first_name
     @last_name = last_name
     @items = []
     @id = Random.rand(1..1_000_000)
+    super(publish_date, archived)
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.add_author(self)
   end
 end
--- game.rb
+++ game.rb
@@ -1,8 +1,14 @@
-class Game
-  attr_accessor :multi_player, :last_played_at
+class Game < Item
+  attr_accessor :multi_player, :last_played_at, :publish_date, :archived
 
-  def initialize(multi_player, last_played_at)
+  def initialize(multi_player, last_played_at, publish_date, archived: false)
     @multi_player = multi_player
     @last_played_at = last_played_at
+    super(publish_date, archived)
+  end
+
+  def can_be_archived?
+    current_date = Date.today
+    super.can_be_archived && last_played_at.year - current_date.year > 2
   end
 end
