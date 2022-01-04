c96e3be656b369900502a027848d15b75b2a81da
Luis Fernando <lfmnovaes@gmail.com>
2022-01-04 15:16:45 -0300

    Add music_album file

---
+++ music_album.rb
@@ -0,0 +1,27 @@
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :publish_date, :on_spotify, :archived
+
+  def initialize(publish_date:, archived: false)
+    super(id: id, publish_date: publish_date, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && on_spotify
+  end
+
+  def to_json(_options = {})
+  {
+    'id' => @id,
+    'genre' => @genre,
+    'author' => @author,
+    'source' => @source,
+    'label' => @label,
+    'publish_date' => @publish_date,
+    'archived' => @archived,
+    'on_spotify' => @on_spotify
+  }
+  end
+end
