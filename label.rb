7e9f529f87a8c0882773c115d6aee33d5c37e4dd
DyaryRaoof <dyary2020@gmail.com>
2022-01-04 18:38:48 +0300

    add Label class

---
+++ label.rb
@@ -0,0 +1,13 @@
+class Label
+    def initialize  title, color
+        @id = Random.rand(0..1000000)
+        @title = title
+        @color = color
+        @items = []
+    end
+
+    def add_item item
+        item.label = self;
+        @items.push(item)
+    end
+end
